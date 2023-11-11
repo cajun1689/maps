3a2a46119edb09d1ceb68460626f116803761d9f
cajun1689 <70188989+cajun1689@users.noreply.github.com>
2023-11-11 13:59:31 -0700

    Update maps.js

--- maps.js
+++ maps.js
@@ -1,38 +1,53 @@
 let map;
 
 function initMap() {
-    // Initialize the map with a default location (update the lat and lng to your city's coordinates)
     map = new google.maps.Map(document.getElementById("map"), {
         center: { lat: 42.8666, lng: -106.3131 }, // Coordinates for Casper, Wyoming
         zoom: 12,
     });
 
-    // Fetch and display the food truck locations
     fetchFoodTruckLocations();
 }
 
 async function fetchFoodTruckLocations() {
     try {
-        // Specify the city as a query parameter
-        const city = "Casper";
+        const city = "casper"; // Lowercase to match the API's expected query parameter
         const apiUrl = `https://ff3d4knxkd.execute-api.us-east-1.amazonaws.com/prod/recent-trucks?city=${city}`;
 
         const response = await fetch(apiUrl);
-        const data = await response.json();
+        const result = await response.json();
 
-        data.forEach(truck => {
-            const position = new google.maps.LatLng(truck.Latitude, truck.Longitude);
+        console.log("Raw response:", result); // Log the raw response
 
-            const marker = new google.maps.Marker({
-                position: position,
-                map: map,
-                title: truck.truckName
-            });
-        });
+        if (response.ok && result.statusCode === 200) {
+            let trucks;
+            try {
+                trucks = JSON.parse(result.body); // Parse the body
+                console.log("Parsed trucks data:", trucks);
+            } catch (e) {
+                console.error('Error parsing response body:', e);
+                return;
+            }
+
+            if (Array.isArray(trucks)) {
+                trucks.forEach(truck => {
+                    const position = new google.maps.LatLng(truck.Latitude, truck.Longitude);
+
+                    new google.maps.Marker({
+                        position: position,
+                        map: map,
+                        title: truck.truckName
+                    });
+                });
+            } else {
+                console.error('Trucks data is not an array:', trucks);
+            }
+        } else {
+            console.error('Error fetching truck data:', result);
+        }
     } catch (error) {
         console.error('Error fetching truck data:', error);
     }
 }
 
-// This event listener will call initMap() when the page loads
 window.addEventListener('load', initMap);
